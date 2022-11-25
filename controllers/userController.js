47a5cc22540e63bf67476646308388630437b1a4
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-24 23:37:03 -0800

    Created userController's create,update,delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const { User, Thought } = require("../models");
 
 module.exports = {
 
-    // For gettin all users
+    // For getting all users
     getUsers(req, res) {
         User.find()
             .then((user) => res.json(user))
@@ -28,4 +28,30 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
 
+    //For updating a user
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "User not found with that ID!" })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // For deleting a user and user's thoughts
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "User not found with that ID!" })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: "Successfully deleted user and user's thoughts" }))
+            .catch((err) => res.status(500).json(err));
+    },
 };
