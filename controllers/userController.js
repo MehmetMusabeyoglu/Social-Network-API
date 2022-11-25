036f27ba1b78ec5c33e1424b9795139209df52e2
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-25 00:13:15 -0800

    Completed user part of the code

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,4 +54,35 @@ module.exports = {
             .then(() => res.json({ message: "Successfully deleted user and user's thoughts" }))
             .catch((err) => res.status(500).json(err));
     },
+
+    // For adding a friend to user
+    addFriend(req, res) {
+        console.log('You are adding a friend');
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "User not found with that ID!" })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    //For removing a friend to user
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "User not found with that ID!" })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 };
