bfb0a88397f8e82a1a0616fcfa9f3eb62b27e5f2
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-28 17:36:27 -0800

    Added routes to thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
 
+const {
+    getThoughts,
+    getThought,
+    createThought,
+    updateThought,
+    removeThought,
+    createReaction,
+    removeReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router.route("/:thoughtId").get(getThought).put(updateThought).delete(removeThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
 module.exports = router;
