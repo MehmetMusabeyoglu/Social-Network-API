ec2c4199b02c75000cd4d4b29f9aff97cb0d21e4
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-11-28 19:31:21 -0800

    Completed thought part of the code

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -35,4 +35,61 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
 
+    // For updating a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "Thought not found with that ID!" })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // For removing a thought and thought's reactions
+    removeThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "Thought not found with that ID!" })
+                    : Thought.deleteMany({ _id: { $in: thought.reactions } })
+            )
+            .then(() => res.json({ message: "Successfully removed thought and thought's reactions" }))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // For creating a reaction to thought
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) => {
+                !thought
+                    ? res.status(404).json({ message: "Thought not found with that ID!" })
+                    : res.json(thought);
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+
+    // For removing a reaction to thought
+    removeReaction(req, res) {
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+            .then((thought) => {
+                !thought
+                    ? res.status(404).json({ message: "Thought not found with that ID!" })
+                    : res.json(thought);
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+
 };
